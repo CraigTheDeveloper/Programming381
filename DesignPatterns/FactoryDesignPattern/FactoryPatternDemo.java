5e3b730676089f6cd6d56ec38ff1d52b8c83d63d
Craig van Straaten <craigvanstraaten464@gmail.com>
2021-02-24 16:57:41 +0200

    Factory design pattern init

---
+++ DesignPatterns/FactoryDesignPattern/FactoryPatternDemo.java
@@ -0,0 +1,9 @@
+package DesignPatterns.FactoryDesignPattern;
+
+public class FactoryPatternDemo {
+    
+    public static void main(String[] args) {
+        // We'll just control the flow of the program here but not the core logic
+
+    }
+}
